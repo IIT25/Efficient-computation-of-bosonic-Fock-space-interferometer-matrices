c434c7bac377a4e39b0db6dd737023dc18762571
JIUHWA <67797035+IIT25@users.noreply.github.com>
2024-10-03 10:01:47 +0200

    refactor

--- python/p/main.py
+++ python/p/main.py
@@ -1,16 +1,7 @@
 import perm
 import numpy as np
-import ctypes 
-cutoff = 3
-arr = {1,2,3,4}
-#initMatrix = np.array([[1, 2], [3, 4]])
-subspace_rep = [None] *3
-subspace_rep_pointer = []
-for i in range(0, cutoff):
-    subspace_rep[i] = np.zeros((i,i))
-    subspace_rep_pointer.append(subspace_rep[i].ctypes.data)
-print(subspace_rep)
-print(subspace_rep_pointer)
-perm.MatrixPointer()
-print(np.array(perm.MatrixAdd(4,5), copy = False))
-print(subspace_rep)
+cutoff = 5
+arr = [[1.0,2.0],[3.0,4.0]]
+nparr = np.array(arr, dtype=np.float64, order='C')
+z = perm.calc_perm(cutoff, nparr)
+print(z)
--- python/p/return.py
+++
@@ -1,7 +0,0 @@
-import perm
-import numpy as np
-cutoff = 5
-arr = [[1.0,2.0],[3.0,4.0]]
-nparr = np.array(arr, dtype=np.float64, order='C')
-z = perm.calc_perm(cutoff, nparr)
-print(z)
